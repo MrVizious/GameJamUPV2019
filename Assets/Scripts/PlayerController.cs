163f6ccfcf65638bcd6f093c70295bc1cddd4bb4
MrVizious <mrviziousgmail.com>
2019-05-06 11:42:40 +0200

    Movimiento terminado

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,44 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private int vidas = 3;
+    private int lives = 3;
+
+    [SerializeField] private KeyCode moveForward;
+    [SerializeField] private KeyCode moveBackward;
+    [SerializeField] private KeyCode moveRight;
+    [SerializeField] private KeyCode moveLeft;
+
+
+
+    public float speedRotation = 250;
+    public float speedForward = 5;
 
     // Update is called once per frame
     void Update()
     {
-        Input.GetAxis("Horizontal");
+        //Forward Movement
+        if (Input.GetKey(moveForward))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speedForward);
+
+
+        }
+        else if (Input.GetKey(moveBackward))
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * -speedForward);
+
+
+        }
+        //Horizontal Rotation
+
+        if (Input.GetKey(moveRight))
+        {
+            transform.Rotate(new Vector3(0, 0, -speedRotation * Time.deltaTime));
+        }
+        else if (Input.GetKey(moveLeft))
+        {
+            transform.Rotate(new Vector3(0, 0, speedRotation * Time.deltaTime));
+        }
+
     }
 }
