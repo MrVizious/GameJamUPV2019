10825ca7683ef4169fa6c6d9c6edc6a3a90b3acc
MrVizious <mrviziousgmail.com>
2019-05-06 11:01:22 +0200

    Script PlayerController creado sin nada hecho

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private int vidas = 3;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Input.GetAxis("Horizontal");
+    }
+}
