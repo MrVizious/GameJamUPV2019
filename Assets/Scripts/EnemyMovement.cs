412c4cea8d2921e3656455e0276f93d0d9429889
MrVizious <mrviziousgmail.com>
2019-05-07 16:18:38 +0200

    Boolean de orientación en enemigos

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -7,11 +7,15 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField] private float speed = 3.5f;
     [SerializeField] private Transform goal;
 
+    public bool goingUp;
+
     
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, goal.position, Time.deltaTime * speed);
+        if(goal.position.y > transform.position.y) goingUp = true;
+        else if(goal.position.y < transform.position.y) goingUp = false;
     }
 
     public void setGoal(Transform t){
