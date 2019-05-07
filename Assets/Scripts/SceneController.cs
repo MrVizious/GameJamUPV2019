256bafbc2ae00e500ce80d2c88caaa19fd8c63be
MrVizious <mrviziousgmail.com>
2019-05-07 15:39:32 +0200

    PauseMenu

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void Restart(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+    }
+
+    public void Quit(){
+        Application.Quit();
+    }
+}
