60a1594827f3f8cdf302bbe623290ba4d6bd8494
MrVizious <mrviziousgmail.com>
2019-05-07 12:23:53 +0200

    Principio de colisión por láser con enemigos

---
+++ Assets/Scripts/EnemyCharacter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCharacter : MonoBehaviour
+{
+    public float health = 1f;
+    public string color;
+
+    public void Hurt(string rayColor){
+        if(rayColor == color) health -= 0.7f * Time.deltaTime;
+        
+    }
+}
--- Assets/Scripts/LaserScript.cs
+++ Assets/Scripts/LaserScript.cs
@@ -17,9 +17,11 @@ public class LaserScript : MonoBehaviour
     private BoxCollider2D collider;
     private LineRenderer line;
     private LayerMask mask;
+    private LayerMask maskEnemies;
     private LineRenderer secondaryLaser;
 
     private string firstColorName;
+    private string secondColorName;
     private Color firstRayColor;
     private Color secondRayColor;
     private string otherRayColor;
@@ -32,7 +34,7 @@ public class LaserScript : MonoBehaviour
     void Start()
     {
         mask = tag == "Laser1" ? 1 << LayerMask.NameToLayer("Laser2") : 1 << LayerMask.NameToLayer("Laser1");
-        //mask = mask || 1 << LayerMask.NameToLayer("Enemy");
+        maskEnemies = 1 << LayerMask.NameToLayer("Enemy");
         collider = GetComponent<BoxCollider2D>();
         line = GetComponent<LineRenderer>();
         secondaryLaser = transform.Find("SecondaryLaser").gameObject.GetComponent<LineRenderer>();
@@ -66,9 +68,65 @@ public class LaserScript : MonoBehaviour
         if(percentageLoad == 0f){
             canShoot = false;
         }
-        // Cast a ray straight forward.
+
+        Debug.Log("Empezando a disparar");
+
+        // Raycast de láser
         RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, raycastDistance, mask);
-        Debug.DrawRay(transform.position, transform.up * 30f, Color.green, 0.2f);
+
+        // Rayo de daño a los enemigos
+        RaycastHit2D hitEnemies = Physics2D.Raycast(transform.position, transform.up, raycastDistance, maskEnemies);
+
+        //Encuentra tanto láser como enemigo
+        if(hitEnemies.collider != null){
+            if(hit.collider !=null){
+                //Si encuentra antes el enemigo que el láser
+                if(Vector2.Distance(transform.position, hitEnemies.point) < Vector2.Distance(transform.position, hit.point) ){
+                    Debug.Log("Ha encontrado a un enemigo antes que a un láser");
+                    secondaryLaser.enabled=false;
+                    line.SetPosition(0, transform.position);
+                    line.SetPosition(1, transform.position + transform.up * Vector2.Distance(transform.position, hitEnemies.point));
+                    this.gameObject.GetComponent<LineRenderer>().enabled=true;
+                    this.gameObject.GetComponent<BoxCollider2D>().enabled=true;
+                }
+                //Si encuentra antes el láser que el enemigo
+                else {
+                    Debug.Log("Ha encontrado a un láser antes que a un enemigo");
+                    MergeLaser(hit);
+                    hitEnemies.collider.gameObject.GetComponent<EnemyCharacter>().Hurt(secondColorName);
+                }
+            }
+            //Si sólo encuentra un enemigo
+            else{
+                Debug.Log("Ha encontrado sólo un enemigo");
+                secondaryLaser.enabled=false;
+                line.SetPosition(0, transform.position);
+                line.SetPosition(1, transform.position + transform.up * Vector2.Distance(transform.position, hitEnemies.point));
+                this.gameObject.GetComponent<LineRenderer>().enabled=true;
+                this.gameObject.GetComponent<BoxCollider2D>().enabled=true;
+                
+                hitEnemies.collider.gameObject.GetComponent<EnemyCharacter>().Hurt(firstColorName);
+            }
+        }
+        //Si sólo encuentra un láser
+        else if(hit.collider != null){
+            Debug.Log("Sólo ha encontrado un láser");
+            MergeLaser(hit);
+        }
+
+        else if(hit.collider == null || hitEnemies.collider == null){
+            Debug.Log("No ha encontrado nada");
+            secondaryLaser.enabled=false;
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, transform.position + transform.up * raycastDistance);
+            this.gameObject.GetComponent<LineRenderer>().enabled=true;
+            this.gameObject.GetComponent<BoxCollider2D>().enabled=true;
+
+        }
+
+        // LAS ANTIGUAS ESCRITURAS HACEN QUE ESTE CÓDIGO FUNCIONE AUNQUE SIN COLISIONES, NO LO JODAS TÚ, POBRE MORTAL
+        /*
+        Debug.DrawRay(transform.position, transform.up * raycastDistance, Color.green, 0.2f);
         setFirstRayColor();
         line.SetColors(firstRayColor,firstRayColor);
         if (hit.collider != null)
@@ -80,10 +138,14 @@ public class LaserScript : MonoBehaviour
         {
         	secondaryLaser.enabled=false;
         	line.SetPosition(0, transform.position);
-            line.SetPosition(1, transform.position + transform.up * 30f);
+            line.SetPosition(1, transform.position + transform.up * raycastDistance);
     		this.gameObject.GetComponent<LineRenderer>().enabled=true;
     		this.gameObject.GetComponent<BoxCollider2D>().enabled=true;
         }
+        */
+        // DE AQUÍ PARA ABAJO YA SON ESCRITURAS ALGO MENOS SAGRADAS
+        
+        
     }
 
     private void MergeLaser(RaycastHit2D hit)
@@ -98,7 +160,7 @@ public class LaserScript : MonoBehaviour
     	this.gameObject.GetComponent<BoxCollider2D>().enabled=true;
 
     	secondaryLaser.SetPosition(0, hit.point);
-		secondaryLaser.SetPosition(1, transform.position + transform.up * 30f);
+		secondaryLaser.SetPosition(1, transform.position + transform.up * raycastDistance);
 		secondaryLaser.SetColors(secondRayColor,secondRayColor);
         secondaryLaser.enabled=true;
     	
@@ -128,18 +190,22 @@ public class LaserScript : MonoBehaviour
     	if(otherRayColor==firstColorName)
     	{
     		secondRayColor=firstRayColor;
+            secondColorName = firstColorName;
     	}
     	else if((otherRayColor == "Azul" && firstColorName=="Rojo")||(otherRayColor == "Rojo" && firstColorName=="Azul"))
     	{//Purpura
     		secondRayColor = new Color(1,0,1,alpha);
+            secondColorName = "Violeta";
     	}
     	else if((otherRayColor == "Azul" && firstColorName=="Amarillo")||(otherRayColor == "Amarillo" && firstColorName=="Azul"))
     	{//Verde
     		secondRayColor = new Color(0,1,0,alpha);
+            secondColorName = "Verde";
     	}
     	else if((otherRayColor == "Amarillo" && firstColorName=="Rojo")||(otherRayColor == "Rojo" && firstColorName=="Amarillo"))
     	{//Naranja
     		secondRayColor = new Color(1,0.5f,0,alpha);
+            secondColorName = "Naranja";
     	}
 
     }
